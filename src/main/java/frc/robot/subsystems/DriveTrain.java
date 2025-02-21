4b3cf0a060607f88a52e3e9e391f65f9a79bd94e
Brandon Klos <bklos89@gmail.com>
2025-02-21 14:25:42 -0500

    Update DriveTrain with SparkMax configuration and encoders

--- src/main/java/frc/robot/subsystems/DriveTrain.java
+++ src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -4,21 +4,86 @@
 
 package frc.robot.subsystems;
 
-import com.ctre.phoenix.motorcontrol.ControlMode;
-import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.config.ClosedLoopConfig.FeedbackSensor;
+import com.revrobotics.spark.config.SparkBaseConfig;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+import com.revrobotics.spark.config.SparkMaxConfig;
+import com.revrobotics.spark.SparkBase.ControlType;
+import com.revrobotics.spark.SparkBase.PersistMode;
+import com.revrobotics.spark.SparkBase.ResetMode;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
 public class DriveTrain extends SubsystemBase {
-  private final WPI_TalonSRX leftFrontMotor = new WPI_TalonSRX(Constants.LEFT_FRONT_DRIVE_MOTOR_CAN_ID);
-  private final WPI_TalonSRX rightFrontMotor = new WPI_TalonSRX(Constants.RIGHT_FRONT_DRIVE_MOTOR_CAN_ID);
-  private final WPI_TalonSRX leftRearMotor = new WPI_TalonSRX(Constants.LEFT_REAR_DRIVE_MOTOR_CAN_ID);
-  private final WPI_TalonSRX rightRearMotor = new WPI_TalonSRX(Constants.RIGHT_REAR_DRIVE_MOTOR_CAN_ID);
+  private final SparkMax leftFrontMotor = new SparkMax(Constants.LEFT_FRONT_DRIVE_MOTOR_CAN_ID, MotorType.kBrushless);
+  private final SparkMax rightFrontMotor = new SparkMax(Constants.RIGHT_FRONT_DRIVE_MOTOR_CAN_ID, MotorType.kBrushless);
+  private final SparkMax leftRearMotor = new SparkMax(Constants.LEFT_REAR_DRIVE_MOTOR_CAN_ID, MotorType.kBrushless);
+  private final SparkMax rightRearMotor = new SparkMax(Constants.RIGHT_REAR_DRIVE_MOTOR_CAN_ID, MotorType.kBrushless);
 
 
   /** Creates a new DriveTrain. */
-  public DriveTrain() {}
+  public DriveTrain() {
+    SparkMaxConfig leftFrontConfig = getLeaderConfig(true);
+    SparkMaxConfig rightFrontConfig = getLeaderConfig(false);
+
+    leftFrontMotor.configure(
+      leftFrontConfig,
+      ResetMode.kNoResetSafeParameters, 
+      PersistMode.kPersistParameters);
+
+    leftRearMotor.configure(
+      getFollowerConfig(leftFrontMotor),
+      ResetMode.kNoResetSafeParameters, 
+      PersistMode.kPersistParameters);
+
+    rightFrontMotor.configure(
+      rightFrontConfig
+          .inverted(false)
+          .idleMode(IdleMode.kBrake), 
+      ResetMode.kNoResetSafeParameters, 
+      PersistMode.kPersistParameters);
+
+    rightRearMotor.configure(
+      getFollowerConfig(rightFrontMotor),
+      ResetMode.kNoResetSafeParameters, 
+      PersistMode.kPersistParameters);
+  }
+
+  private SparkMaxConfig getLeaderConfig(boolean inverted) {
+    // https://www.chiefdelphi.com/t/motion-magic-for-spark-max/472664/6
+    SparkMaxConfig config = new SparkMaxConfig();
+    config.encoder
+          .positionConversionFactor(1.0)
+          .velocityConversionFactor(1.0);
+    // leftFrontConfig.closedLoopRampRate(1.0); // One second to go full throttle
+    config.closedLoop
+        .feedbackSensor(FeedbackSensor.kPrimaryEncoder)
+        .p(0.5)
+        .i(0.0)
+        .d(0.0)
+        .velocityFF(0.0)
+        .maxOutput(1.0)
+        .minOutput(-1.0)
+        .maxMotion
+            .maxAcceleration(1000.0)
+            .maxVelocity(2000.0)
+            .allowedClosedLoopError(0.1);
+    config
+        .idleMode(IdleMode.kBrake)
+        .smartCurrentLimit(40)
+        .inverted(inverted);
+    return config;
+  }
+
+  private SparkBaseConfig getFollowerConfig(SparkMax leader) {
+    return new SparkMaxConfig()
+      .idleMode(IdleMode.kBrake)
+      .smartCurrentLimit(40)
+      .follow(leader);
+  }
 
   @Override
   public void periodic() {
@@ -26,13 +91,14 @@ public class DriveTrain extends SubsystemBase {
   }
 
   public void moveLeftMotors(double speed) {
-    leftFrontMotor.set(ControlMode.PercentOutput, speed);
-    leftRearMotor.set(ControlMode.PercentOutput, speed);
+    leftFrontMotor.set(speed);
+
+    // Control speed - hopefully should be smoother
+    // leftFrontMotor.getClosedLoopController().setReference(speed, ControlType.kMAXMotionPositionControl);
   }
   
   public void moveRightMotors(double speed) {
-    rightFrontMotor.set(ControlMode.PercentOutput, -speed);
-    rightRearMotor.set(ControlMode.PercentOutput, -speed);
+    rightFrontMotor.set(-speed);
   }
 
   public void stop() {
