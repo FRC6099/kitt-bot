18604490d3588b3714d27c878f082c0a6780b889
knightriders6099 <knightriders6099@gmail.com>
2026-02-28 10:42:11 -0500

    clean up team repo code copy maxswerve cod over to team

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,116 +4,80 @@
 
 package frc.robot;
 
-import frc.robot.commands.DetectArmPosition;
-import frc.robot.commands.EjectFieldElement;
-import frc.robot.commands.ExtendIntakeArm;
-import frc.robot.commands.InjectFieldElement;
-import frc.robot.commands.OperateClimber;
-import frc.robot.commands.RetractIntakeArm;
-import frc.robot.commands.TankDrive;
-import frc.robot.commands.autonomous.DriveBackward;
-import frc.robot.commands.autonomous.DriveForward;
-import frc.robot.commands.autonomous.EjectFieldElementAndMove;
-import frc.robot.commands.autonomous.EjectFieldElementAndMoveAngle;
-import frc.robot.commands.autonomous.EjectFieldElementAndMoveCustom;
-import frc.robot.commands.autonomous.EjectTwoFieldElementsSequence;
-import frc.robot.controllers.ClimberController;
-import frc.robot.controllers.TankDriveController;
-import frc.robot.subsystems.Climber;
-import frc.robot.subsystems.DriveTrain;
-import frc.robot.subsystems.Intake;
-import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.controller.ProfiledPIDController;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.trajectory.Trajectory;
+import edu.wpi.first.math.trajectory.TrajectoryConfig;
+import edu.wpi.first.math.trajectory.TrajectoryGenerator;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.PS4Controller.Button;
+import frc.robot.Constants.AutoConstants;
+import frc.robot.Constants.DriveConstants;
+import frc.robot.Constants.OIConstants;
+import frc.robot.subsystems.DriveSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.WaitCommand;
-import edu.wpi.first.wpilibj2.command.button.CommandJoystick;
-import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
-import edu.wpi.first.wpilibj2.command.button.Trigger;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.RunCommand;
+import edu.wpi.first.wpilibj2.command.SwerveControllerCommand;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+import java.util.List;
 
-/**
- * This class is where the bulk of the robot should be declared. Since Command-based is a
+/*
+ * This class is where the bulk of the robot should be declared.  Since Command-based is a
  * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
- * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
- * subsystems, commands, and trigger mappings) should be declared here.
+ * periodic methods (other than the scheduler calls).  Instead, the structure of the robot
+ * (including subsystems, commands, and button mappings) should be declared here.
  */
 public class RobotContainer {
-  // The robot's subsystems and commands are defined here...
-  private final DriveTrain driveTrain = new DriveTrain();
-  private final Intake intake = new Intake();
-  private final Climber climber = new Climber();
+  // The robot's subsystems
+  private final DriveSubsystem m_robotDrive = new DriveSubsystem();
 
-  // Replace with CommandPS4Controller or CommandJoystick if needed
-  private final CommandXboxController xboxController = new CommandXboxController(Constants.XBOX_CONTROLLER_USB_ID);
-  private final CommandJoystick leftJoystick = new CommandJoystick(Constants.LEFT_JOYSTICK_USB_ID);
-  private final CommandJoystick rightJoystick = new CommandJoystick(Constants.RIGHT_JOYSTICK_USB_ID);
+  // The driver's controller
+  XboxController m_driverController = new XboxController(OIConstants.kDriverControllerPort);
 
-  // Default commands
-  private final TankDrive tankDrive = new TankDrive(driveTrain, new TankDriveController(leftJoystick, rightJoystick));
-  private final ExtendIntakeArm extendIntakeArm = new ExtendIntakeArm(intake);
-  private final RetractIntakeArm retractIntakeArm = new RetractIntakeArm(intake);
-  private final InjectFieldElement injectFieldElement = new InjectFieldElement(intake);
-  private final EjectFieldElement ejectFieldElement = new EjectFieldElement(intake);
-  private final OperateClimber operateClimber = new OperateClimber(new ClimberController(xboxController), climber);
-  private final DetectArmPosition detectArmPosition = new DetectArmPosition(intake);
-
-  // Autonomous Commands
-  // Add ability to choose autonomous mode in SmartDashboard
-  private final SendableChooser<Command> autonomousChooser = new SendableChooser<>();
-
-  /** The container for the robot. Contains subsystems, OI devices, and commands. */
+  /**
+   * The container for the robot. Contains subsystems, OI devices, and commands.
+   */
   public RobotContainer() {
-    // Configure the trigger bindings
-    configureBindings();
-    configureSubsystems();
-    configureAutonomousModes();
-  }
+    // Configure the button bindings
+    configureButtonBindings();
 
-  private void configureSubsystems() {
-    this.driveTrain.setDefaultCommand(tankDrive);
-    this.climber.setDefaultCommand(operateClimber);
-    this.intake.setDefaultCommand(detectArmPosition);
-  }
-
-  private void configureAutonomousModes() {
-    this.autonomousChooser.addOption("Do nothing", new WaitCommand(10.0));
-    this.autonomousChooser.addOption("Drive Backwards", new DriveBackward(driveTrain, 2.0));
-    this.autonomousChooser.addOption("Drive Forward", new DriveForward(driveTrain, 2.0));
-    this.autonomousChooser.setDefaultOption("Score coral close", new EjectFieldElementAndMove(intake, driveTrain, 1.5));
-    this.autonomousChooser.addOption("Score coral far", new EjectFieldElementAndMove(intake, driveTrain, 3.4));
-
-    
-    SmartDashboard.putNumber("Custom Auton Drive Forward", 1.5);
-    this.autonomousChooser.addOption("Score coral custom", new EjectFieldElementAndMoveCustom(intake, driveTrain));
-
-    // this.autonomousChooser.addOption("Score two notes", new EjectTwoFieldElementsSequence(intake, driveTrain));
-    SmartDashboard.putData("Autonomous Options", this.autonomousChooser);
-    // SmartDashboard.putNumber("Autonomous Number", 0);
+    // Configure default commands
+    m_robotDrive.setDefaultCommand(
+        // The left stick controls translation of the robot.
+        // Turning is controlled by the X axis of the right stick.
+        new RunCommand(
+            () -> m_robotDrive.drive(
+                -MathUtil.applyDeadband(m_driverController.getLeftY(), OIConstants.kDriveDeadband),
+                -MathUtil.applyDeadband(m_driverController.getLeftX(), OIConstants.kDriveDeadband),
+                -MathUtil.applyDeadband(m_driverController.getRightX(), OIConstants.kDriveDeadband),
+                true),
+            m_robotDrive));
   }
 
   /**
-   * Use this method to define your trigger->command mappings. Triggers can be created via the
-   * {@link Trigger#Trigger(java.util.function.BooleanSupplier)} constructor with an arbitrary
-   * predicate, or via the named factories in {@link
-   * edu.wpi.first.wpilibj2.command.button.CommandGenericHID}'s subclasses for {@link
-   * CommandXboxController Xbox}/{@link edu.wpi.first.wpilibj2.command.button.CommandPS4Controller
-   * PS4} controllers or {@link edu.wpi.first.wpilibj2.command.button.CommandJoystick Flight
-   * joysticks}.
+   * Use this method to define your button->command mappings. Buttons can be
+   * created by
+   * instantiating a {@link edu.wpi.first.wpilibj.GenericHID} or one of its
+   * subclasses ({@link
+   * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then calling
+   * passing it to a
+   * {@link JoystickButton}.
    */
-  private void configureBindings() {
-    // Schedule `ExampleCommand` when `exampleCondition` changes to `true`
-    // new Trigger(m_exampleSubsystem::exampleCondition)
-    //     .onTrue(new ExampleCommand(m_exampleSubsystem));
+  private void configureButtonBindings() {
+    new JoystickButton(m_driverController, Button.kR1.value)
+        .whileTrue(new RunCommand(
+            () -> m_robotDrive.setX(),
+            m_robotDrive));
 
-    // Schedule `exampleMethodCommand` when the Xbox controller's B button is pressed,
-    // cancelling on release.
-    // m_driverController.b().whileTrue(m_exampleSubsystem.exampleMethodCommand());
-    xboxController.leftBumper().whileTrue(extendIntakeArm);
-    xboxController.rightBumper().whileTrue(retractIntakeArm);
-    xboxController.leftTrigger().whileTrue(injectFieldElement);
-    xboxController.rightTrigger().whileTrue(ejectFieldElement);
-    // xboxController.a()
-    // .whileTrue(new RunCommand(() -> shooter.prime(), shooter))
-    // .whileFalse(new RunCommand(() -> shooter.stop(), shooter));
+    new JoystickButton(m_driverController, XboxController.Button.kStart.value)
+        .onTrue(new InstantCommand(
+            () -> m_robotDrive.zeroHeading(),
+            m_robotDrive));
   }
 
   /**
@@ -122,7 +86,43 @@ public class RobotContainer {
    * @return the command to run in autonomous
    */
   public Command getAutonomousCommand() {
-    // An example command will be run in autonomous
-    return autonomousChooser.getSelected();
+    // Create config for trajectory
+    TrajectoryConfig config = new TrajectoryConfig(
+        AutoConstants.kMaxSpeedMetersPerSecond,
+        AutoConstants.kMaxAccelerationMetersPerSecondSquared)
+        // Add kinematics to ensure max speed is actually obeyed
+        .setKinematics(DriveConstants.kDriveKinematics);
+
+    // An example trajectory to follow. All units in meters.
+    Trajectory exampleTrajectory = TrajectoryGenerator.generateTrajectory(
+        // Start at the origin facing the +X direction
+        new Pose2d(0, 0, new Rotation2d(0)),
+        // Pass through these two interior waypoints, making an 's' curve path
+        List.of(new Translation2d(1, 1), new Translation2d(2, -1)),
+        // End 3 meters straight ahead of where we started, facing forward
+        new Pose2d(3, 0, new Rotation2d(0)),
+        config);
+
+    var thetaController = new ProfiledPIDController(
+        AutoConstants.kPThetaController, 0, 0, AutoConstants.kThetaControllerConstraints);
+    thetaController.enableContinuousInput(-Math.PI, Math.PI);
+
+    SwerveControllerCommand swerveControllerCommand = new SwerveControllerCommand(
+        exampleTrajectory,
+        m_robotDrive::getPose, // Functional interface to feed supplier
+        DriveConstants.kDriveKinematics,
+
+        // Position controllers
+        new PIDController(AutoConstants.kPXController, 0, 0),
+        new PIDController(AutoConstants.kPYController, 0, 0),
+        thetaController,
+        m_robotDrive::setModuleStates,
+        m_robotDrive);
+
+    // Reset odometry to the starting pose of the trajectory.
+    m_robotDrive.resetOdometry(exampleTrajectory.getInitialPose());
+
+    // Run path following command, then stop at the end.
+    return swerveControllerCommand.andThen(() -> m_robotDrive.drive(0, 0, 0, false));
   }
 }
